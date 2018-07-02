63e036369c9fb8fb1123c2f4412e5f024286210f
Miguel Gomes <jmbg.41@gmail.com>
2018-07-02 09:39:50 +0100

    Add Tests to ApplicationStatusService.getApplicationStatus

--- ApplicationsDashboard-Core/src/test/java/com/apps/dashboard/services/impl/ApplicationStatusServiceImplTest.java
+++ ApplicationsDashboard-Core/src/test/java/com/apps/dashboard/services/impl/ApplicationStatusServiceImplTest.java
@@ -1,7 +1,9 @@
 package com.apps.dashboard.services.impl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -10,6 +12,7 @@ import static org.mockito.Mockito.when;
 import com.apps.dashboard.exceptions.EntityNotFoundException;
 import com.apps.dashboard.model.ServiceInfo;
 import com.apps.dashboard.repositories.ApplicationStatusRepo;
+import java.util.Optional;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -91,6 +94,58 @@ public class ApplicationStatusServiceImplTest {
     assertThrows(EntityNotFoundException.class, () -> {
       applicationStatusService.updateApplicationStatus(applicationID, serviceInfo);
     });
+  }
+
+  /**
+   * Test getApplicationStatus
+   */
+  @Test
+  public void testGetApplicationStatus() {
+
+    final String applicationID = "123";
+    final String appVersion = "v1";
+    final boolean health = true;
+
+    final ServiceInfo serviceInfo = ServiceInfo.builder()
+        .applicationId(applicationID)
+        .version(appVersion)
+        .healthy(health)
+        .build();
+
+    when(this.applicationStatusRepo.getApplicationStatus(eq(applicationID)))
+        .thenReturn(serviceInfo);
+
+    Optional<ServiceInfo> serviceInfoOpt = this.applicationStatusService.getApplicationStatus(applicationID);
+
+    assertTrue(serviceInfoOpt.isPresent());
+
+    serviceInfoOpt.ifPresent(newServiceInfo -> {
+      assertEquals(applicationID, newServiceInfo.getApplicationId());
+      assertEquals(appVersion, newServiceInfo.getVersion());
+      assertEquals(health, newServiceInfo.isHealthy());
+    });
+  }
+
+  @Test
+  public void testGetApplicationStatusWithBlankId() {
+    final String applicationID = StringUtils.EMPTY;
+
+    assertThrows(IllegalArgumentException.class, () -> {
+      applicationStatusService.getApplicationStatus(applicationID);
+    });
+  }
+
+  @Test
+  public void testGetApplicationStatusWithNonExistentId() {
+
+    final String applicationID = "nonExistentId";
+
+    when(this.applicationStatusRepo.getApplicationStatus(eq(applicationID)))
+        .thenReturn(null);
+
+    Optional<ServiceInfo> serviceInfo = this.applicationStatusService.getApplicationStatus(applicationID);
+
+    assertFalse(serviceInfo.isPresent());
   }
 
 }
