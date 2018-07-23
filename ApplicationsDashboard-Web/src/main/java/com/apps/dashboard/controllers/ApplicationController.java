7b7d520e39a93f0db3a517717def1765fe18c04b
Miguel Gomes <jmbg.41@gmail.com>
2018-07-23 08:34:13 +0100

    Add Visual information regarding service status

--- ApplicationsDashboard-Web/src/main/java/com/apps/dashboard/controllers/ApplicationController.java
+++ ApplicationsDashboard-Web/src/main/java/com/apps/dashboard/controllers/ApplicationController.java
@@ -1,8 +1,13 @@
 package com.apps.dashboard.controllers;
 
 import com.apps.dashboard.model.Application;
+import com.apps.dashboard.model.ServiceInfo;
 import com.apps.dashboard.services.ApplicationService;
+import com.apps.dashboard.services.ApplicationStatusService;
 import java.util.Collection;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,11 +16,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ApplicationController {
 
-  private ApplicationService applicationService;
+  private final ApplicationService applicationService;
+
+  private final ApplicationStatusService applicationStatusService;
 
   @Autowired
-  public ApplicationController(ApplicationService applicationService) {
+  public ApplicationController(ApplicationService applicationService,
+      ApplicationStatusService applicationStatusService) {
     this.applicationService = applicationService;
+    this.applicationStatusService = applicationStatusService;
   }
 
   @GetMapping("/applications")
@@ -23,9 +32,21 @@ public class ApplicationController {
 
     Collection<Application> applications = this.applicationService.getAllApplications();
 
+    Map<Long, ServiceInfo> serviceInfoMap = applications.stream()
+        .map(app -> this.applicationStatusService.getApplicationStatus(app.getId())
+            .orElseGet(() -> this.createEmptyServiceInfo(app)))
+        .collect(Collectors.toMap(ServiceInfo::getApplicationId, Function.identity()));
+
     model.addAttribute("apps", applications);
+    model.addAttribute("serviceInfoMap", serviceInfoMap);
 
     return "applications";
+  }
+
+  private ServiceInfo createEmptyServiceInfo(Application app) {
+    return ServiceInfo.builder()
+        .applicationId(app.getId())
+        .build();
   }
 
 }
