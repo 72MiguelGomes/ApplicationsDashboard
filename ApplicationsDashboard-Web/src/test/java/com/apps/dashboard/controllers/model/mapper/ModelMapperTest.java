2129f93c3f01aec8b008fd871327b82a43993447
Miguel Gomes <jmbg.41@gmail.com>
2018-07-30 19:09:26 +0100

    Test convert to Application Model with application status

--- ApplicationsDashboard-Web/src/test/java/com/apps/dashboard/controllers/model/mapper/ModelMapperTest.java
+++ ApplicationsDashboard-Web/src/test/java/com/apps/dashboard/controllers/model/mapper/ModelMapperTest.java
@@ -1,8 +1,12 @@
 package com.apps.dashboard.controllers.model.mapper;
 
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
 import com.apps.dashboard.controllers.model.ApplicationModel;
 import com.apps.dashboard.model.Application;
 import com.apps.dashboard.services.ApplicationStatusService;
+import java.util.Optional;
 import name.falgout.jeffrey.testing.junit.mockito.MockitoExtension;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -23,7 +27,37 @@ class ModelMapperTest {
    * Test convertApplication
    */
   @Test
-  public void testConvertApplication() {
+  public void testConvertApplicationWithoutAppStatus() {
+    final Long id = 123L;
+    final String applicationName = "appName";
+    final String dns = "https:localhost.com";
+    final String healthCheckEndpoint = "/ping";
+
+    final Application application = Application.builder()
+        .id(id)
+        .name(applicationName)
+        .dns(dns)
+        .healthEndpoint(healthCheckEndpoint)
+        .build();
+
+    when(this.applicationStatusService.getApplicationStatus(eq(id)))
+        .thenReturn(Optional.empty());
+
+    ApplicationModel applicationModel = this.modelMapper.convertApplication(application);
+
+    Assertions.assertEquals(id, applicationModel.getId());
+    Assertions.assertEquals(applicationName, applicationModel.getName());
+    Assertions.assertEquals(dns, applicationModel.getDns());
+    Assertions.assertEquals(healthCheckEndpoint, applicationModel.getHealthEndpoint());
+    Assertions.assertNull(applicationModel.getVersion());
+    Assertions.assertFalse(applicationModel.isHealthy());
+  }
+
+  /**
+   * Test convertApplication
+   */
+  @Test
+  public void testConvertApplicationModel() {
     final Long id = 123L;
     final String applicationName = "appName";
     final String dns = "https:localhost.com";
